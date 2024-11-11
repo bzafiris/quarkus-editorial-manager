bad76304d0ad46b8a0f919ccb776794343bfdfbe
Bill Zafeiris <bzafiris@gmail.com>
2024-11-11 10:36:00 +0200

    Test Review persistence

--- src/test/java/gr/aueb/edtmgr/persistence/ArticleJPATest.java
+++ src/test/java/gr/aueb/edtmgr/persistence/ArticleJPATest.java
@@ -107,4 +107,64 @@ public class ArticleJPATest extends JPATest {
         return article;
     }
 
+    private Article fetchArticleWithReviewInvitationsAndReviewers(String researcherEmail) {
+        // fetch article with correspondent author and review invitations
+        // Notice left join fetch on review invitations, it is required
+        // since review invitations collection may be empty
+        Query query = em.createQuery("select a from Article a " +
+                "left join fetch a.reviewInvitations inv " +
+                "join fetch inv.reviewer " +
+                "join fetch a.correspondentAuthor r " +
+                "where r.personalInfo.email=:email");
+
+        query.setParameter("email", researcherEmail);
+        List<Article> result = query.getResultList();
+        assertEquals(1, result.size());
+        Article article = result.get(0);
+        return article;
+    }
+
+    @Test
+    public void persistArticleReview(){
+
+        createAndPersistReviewInvitationForUser("mgia@aueb.gr");
+
+        // start new session
+        em = JPAUtil.getCurrentEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Article article = fetchArticleWithReviewInvitationsAndReviewers("ndia@aueb.gr");
+        Researcher researcher = fetchResearcherByEmail("mgia@aueb.gr");
+
+        Review review = article.createReview(researcher, 70, "Author comments",
+                "editor comments", Recommendation.ACCEPT);
+
+        em.persist(review);
+        assertNotNull(review.getId());
+        tx.commit();
+        // clear cache, all results will be requested from db
+        em.clear();
+
+        Review savedReview = (Review) em.createQuery("select r from Review r").getSingleResult();
+        assertNotNull(savedReview);
+
+        assertEquals(researcher.getId(), savedReview.getInvitation().getReviewer().getId());
+
+    }
+
+    private void createAndPersistReviewInvitationForUser(String email) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Article article = fetchArticleWithReviewInvitations("ndia@aueb.gr");
+        Researcher researcher = fetchResearcherByEmail(email);
+
+        ReviewInvitation invitation = article.inviteReviewer(researcher);
+        invitation.accept();
+        tx.commit();
+
+        assertNotNull(invitation.getId());
+        em.close();
+    }
+
 }
